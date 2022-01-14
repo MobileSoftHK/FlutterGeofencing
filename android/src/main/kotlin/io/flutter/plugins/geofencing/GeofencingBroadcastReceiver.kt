88d43f5ba6370cbaa9b21754d0ec676f9de882e6
Ondřej Boura <ondraboura@gmail.com>
2022-01-14 10:34:39 +0100

    Refactoring

--- android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingBroadcastReceiver.kt
+++ android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingBroadcastReceiver.kt
@@ -7,7 +7,8 @@ package io.flutter.plugins.geofencing
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import io.flutter.FlutterInjector
+import io.flutter.embedding.engine.loader.FlutterLoader
 import io.flutter.view.FlutterMain
 
 
@@ -15,9 +16,10 @@ class GeofencingBroadcastReceiver : BroadcastReceiver() {
     companion object {
         private const val TAG = "GeofencingBroadcastReceiver"
     }
+
     override fun onReceive(context: Context, intent: Intent) {
-        FlutterMain.startInitialization(context)
-        FlutterMain.ensureInitializationComplete(context, null)
+        FlutterInjector.instance().flutterLoader().startInitialization(context)
+        FlutterInjector.instance().flutterLoader().ensureInitializationComplete(context, null)
         GeofencingService.enqueueWork(context, intent)
     }
 }
