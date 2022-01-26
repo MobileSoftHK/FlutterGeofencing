3b1e82cf104c5fd8c401abaca70e102ad94f3bd8
Ondřej Boura <ondraboura@gmail.com>
2022-01-26 07:02:42 +0100

    Removed Android request for location permission inside initializeService

--- android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingPlugin.kt
+++ android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingPlugin.kt
@@ -271,18 +271,6 @@ class GeofencingPlugin : ActivityAware, FlutterPlugin, MethodCallHandler {
         val args = call.arguments<ArrayList<*>>()
         when (call.method) {
             "GeofencingPlugin.initializeService" -> {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    mActivity?.requestPermissions(
-                            arrayOf(
-                                    Manifest.permission.ACCESS_FINE_LOCATION,
-                                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
-                            ), 12312
-                    )
-                } else
-                    mActivity?.requestPermissions(
-                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                            12312
-                    )
                 initializeService(mContext!!, args)
                 result.success(true)
             }
